4cac192350a7ffcc3755a175dfe0aa5e3ce101f8
dan.vsm <dan.vsm@gmail.com>
2023-06-03 14:36:47 -0300

    Criando list

--- JavaScript/mochila-de-viagem/main.js
+++ JavaScript/mochila-de-viagem/main.js
@@ -1,8 +1,25 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById('lista')
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     console.log(e)
-    console.log(e.target.elements['nome'].value)
-    console.log(e.target.elements['quantidade'].value)
+    criaElemento(e.target.elements['nome'].value, e.target.elements['quantidade'].value)
 })
+
+function criaElemento (nome, quantidade) {
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+    
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    
+    lista.appendChild(novoItem)
+
+    console.log(numeroItem)
+}
