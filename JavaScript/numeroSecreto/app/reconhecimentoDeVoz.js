3849ea21567642860b5b7d8d92851fa3a2537e61
dan.vsm <125106450+Danvsm@users.noreply.github.com>
2023-06-18 19:23:09 -0300

    chute

--- JavaScript/numeroSecreto/app/reconhecimentoDeVoz.js
+++ JavaScript/numeroSecreto/app/reconhecimentoDeVoz.js
@@ -1,25 +1,24 @@
 const elementoChute = document.getElementById('chute')
 
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-
-recognition.lang = 'pt-BR'
+recognition.lang = 'pt-Br'
 recognition.start()
 
-
-recognition.addEventListerner('result', onSpeak())
+recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
     chute = e.results[0][0].transcript
     exibeChuteNaTela(chute)
-    
-
+    verificaSeOChutePossuiUmValorValido(chute)
 }
 
 function exibeChuteNaTela(chute) {
-     elementoChute.innerHTML = `
-     <div>Você disse: </div> 
-     <span class="box"> ${chute} </span>`
-     console.log("estou funcionando")
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
 }
+
+recognition.addEventListener('end', () => recognition.start())
